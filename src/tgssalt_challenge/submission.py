bf3d5ad3b391d00e5d0efb19e627cb86dba42f66
Mark <mark_a_hewitt@yahoo.co.uk>
2018-09-17 08:17:43 +0200

    Added UNet Reset example, additional eda

--- src/tgssalt_challenge/submission.py
+++ src/tgssalt_challenge/submission.py
@@ -8,45 +8,6 @@ import numpy as np
 # from tgssalt_challenge.io import IO
 
 
-# Source https://www.kaggle.com/bguberfain/unet-with-depth
-def RLenc(img, order='F', format=True):
-    """
-    img is binary mask image, shape (r,c)
-    order is down-then-right, i.e. Fortran
-    format determines if the order needs to be preformatted (according to submission rules) or not
-
-    returns run length as an array or string (if format is True)
-    """
-    bytes = img.reshape(img.shape[0] * img.shape[1], order=order)
-    runs = []  ## list of run lengths
-    r = 0  ## the current run length
-    pos = 1  ## count starts from 1 per WK
-    for c in bytes:
-        if (c == 0):
-            if r != 0:
-                runs.append((pos, r))
-                pos += r
-                r = 0
-            pos += 1
-        else:
-            r += 1
-
-    # if last run is unsaved (i.e. data ends with 1)
-    if r != 0:
-        runs.append((pos, r))
-        pos += r
-        r = 0
-
-    if format:
-        z = ''
-
-        for rr in runs:
-            z += '{} {} '.format(rr[0], rr[1])
-        return z[:-1]
-    else:
-        return runs
-
-
 def rle_decode(rle_mask):
     """
     rle_mask: run-length as string formated (start length)
@@ -66,14 +27,19 @@ def rle_decode(rle_mask):
     return img.reshape(101, 101)
 
 
-def rle_encode(im):
+def rle_encode(im, order='F', format=True):
     """
     used for converting the decoded image to rle mask
     im: numpy array, 1 - mask, 0 - background
+    order is down-then-right, i.e. Fortran
+    format determines if the order needs to be preformatted (according to submission rules) or not
     Returns run length as string formated
     """
-    pixels = im.flatten()
+    pixels = im.flatten(order=order)
     pixels = np.concatenate([[0], pixels, [0]])
     runs = np.where(pixels[1:] != pixels[:-1])[0] + 1
     runs[1::2] -= runs[::2]
-    return ' '.join(str(x) for x in runs)
+    if format:
+        return ' '.join(str(x) for x in runs)
+    else:
+        return runs

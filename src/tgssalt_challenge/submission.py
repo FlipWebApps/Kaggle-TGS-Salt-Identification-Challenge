79a7487f24b6e19bfbd668587269960d1e6902fe
Mark <mark_a_hewitt@yahoo.co.uk>
2018-09-25 22:58:22 +0200

    Added enselbming code and notebook

--- src/tgssalt_challenge/submission.py
+++ src/tgssalt_challenge/submission.py
@@ -2,13 +2,26 @@
 This module contains Kaggle submission related code.
 
 """
+import os
+from functools import reduce
+
 import numpy as np
+import pandas as pd
 
 # from tgssalt_challenge import features
 # from tgssalt_challenge.io import IO
 
 
 def rle_decode(rle_mask):
+    """Decode a run length encoded string.
+
+    Args:
+        rle_mask: run-length as string formated (start length)
+
+    Returns:
+        mask (numpy array): 1 - mask, 0 - background
+    """
+
     """
     rle_mask: run-length as string formated (start length)
     shape: (height,width) of array to return
@@ -28,12 +41,15 @@ def rle_decode(rle_mask):
 
 
 def rle_encode(im, order='F', format=True):
-    """
-    used for converting the decoded image to rle mask
-    im: numpy array, 1 - mask, 0 - background
-    order is down-then-right, i.e. Fortran
-    format determines if the order needs to be preformatted (according to submission rules) or not
-    Returns run length as string formated
+    """Decode a numpy array into a run length encoded string.
+
+    Args:
+        im: numpy array where 1 is a mask value and 0 is background.
+        order: is down-then-right, i.e. Fortran
+        format: determines if the order needs to be preformatted (according to submission rules) or not
+
+    Returns:
+        lre_encoded (string): run length as string formated
     """
     pixels = im.flatten(order=order)
     pixels = np.concatenate([[0], pixels, [0]])
@@ -43,3 +59,47 @@ def rle_encode(im, order='F', format=True):
         return ' '.join(str(x) for x in runs)
     else:
         return runs
+
+
+def ensemble_rle_submissions(submission_files, threshold):
+    """Ensemble multiple rle submission files.
+
+    Args:
+        submission_files: list of files to ensemble
+        threshold: number of matches needed to across submissions to accept a mask pixel
+
+    Returns:
+        result (dataframe): an ensembled result file
+        df (dataframe): merged dataframe.
+    """
+    dataframes = []
+    for file in submission_files:
+        df = pd.read_csv(file).rename(
+            columns={'rle_mask': 'rle_mask_' + str(len(dataframes))})
+        dataframes.append(df)
+
+    df = reduce(lambda left,right: pd.merge(left, right, on=['id']), dataframes)\
+
+    result = dataframes[0].copy().rename(columns={'rle_mask_0': 'rle_mask'})
+
+    for i in range(len(result)):
+        nan_count = 0
+        for mi in range(len(dataframes)):
+            if str(df.loc[i, 'rle_mask_' + str(mi)]) == str(np.nan):
+                nan_count += 1
+
+        if nan_count == 0:
+            decoded_mask_all = rle_decode(df.loc[i, 'rle_mask_0'])
+            for mi in range(1, len(dataframes)):
+                decoded_mask_all += rle_decode(df.loc[i, 'rle_mask_' + str(mi)])
+
+            decoded_mask_all[decoded_mask_all < threshold] = 0
+            decoded_mask_all[decoded_mask_all >= threshold] = 1
+
+            mask = rle_encode(decoded_mask_all)
+
+            result.loc[i, 'rle_mask'] = mask
+
+        i = i + 1
+
+    return result, df
